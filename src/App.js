763a25d3e3f864b084260055b985efcea21ea681
Ishan Jain <ishanjain28@gmail.com>
2016-06-23 12:57:29 +0530

    Finshed Layout and functionality of Get location button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import $ from '../js/jquery-2.2.4.min.js';
 
+
+const RootBlock = React.createClass({
+    render: function()  {
+        return (
+            <div>
+                <Navbar />
+                <FrontLandingWrapper />
+            </div>
+        )
+    }
+});
 const Navbar = React.createClass({
+
     render: function () {
         return (
             <div className="navBar" id="navBar">
@@ -11,13 +23,58 @@ const Navbar = React.createClass({
     }
 });
 
-const EnterLocation = React.createClass({
+const FrontLandingWrapper = React.createClass({
     render: function()  {
         return (
-            <div className="EnterLocation" id="EnterLocation">
-                <LocationInput />
-                <br />
-                <GetWeatherBtn />
+            <div className="FrontLandingWrapper">
+                <EnterLocation />
+                <SpinnerBlock />
+            </div>
+        )
+    }
+});
+
+const EnterLocation = React.createClass({
+    getInitialState: function () {
+        return {
+            latitude: '',
+            longitude: ''
+        }
+    },
+    success: function(pos) {
+        this.setState({
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude
+        });
+        localStorage.coords = pos.coords.toString();
+    },
+    fail: function (err)    {
+        console.error('Error(' + err.code + '): ' + err.message)
+        this.setState({
+            latitude: null,
+            longitude: null
+        });
+    },
+    getLocation: function() {
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 12000
+        };
+
+        if(navigator.geolocation)   {
+            navigator.geolocation.getCurrentPosition(this.success, this.fail, options);
+        } else {
+            console.error('navigator.geolocation not found');
+        }
+    },
+    render: function()  {
+        return (
+            <div className="EnterLocationWrapper">
+                <div className="EnterLocation" id="EnterLocation">
+                    <LocationInput getLocation={this.getLocation}/>
+                    <GetWeatherBtn lat={this.state.latitude} long={this.state.longitude} />
+                </div>
+                <InputStatus lat={this.state.latitude} long={this.state.longitude}/>
             </div>
         );
     }
@@ -26,7 +83,7 @@ const EnterLocation = React.createClass({
 const LocationInput = React.createClass({
     getInitialState: function() {
         return {
-            LocationInputText: "Example: 110001,IN"
+            LocationInputText: ""
         };
     },
     handleChange: function(e)    {
@@ -44,28 +101,55 @@ const LocationInput = React.createClass({
                     value={this.state.LocationInputText}
                     onChange={this.handleChange}
                     className="LocationInputText"
+                    placeholder="Example: 110001,IN"
                     />
-                <GetLocationBtn />
+                <GetLocationBtn getLocation={this.props.getLocation}/>
             </div>
         </div>
         );
     }
 });
+
 const GetLocationBtn = React.createClass({
     render: function()  {
         return (
             <div className="GetLocationBtn">
-                <i className="location-icon fa fa-location-arrow fa-2x"></i>
+                <a href="#" onClick={this.props.getLocation}>
+                    <i className="location-icon fa fa-location-arrow fa-2x"></i>
+                </a>
             </div>
         );
     }
 });
 
 const GetWeatherBtn = React.createClass({
+    getInitialState: function() {
+        return {
+            weatherData: ""
+        };
+    },
+    getWeather: function()  {
+        if(this.props.lat && this.props.long)   {
+            var key = "";
+            var url = "";
+            $.ajax({
+                url: url,
+                dataType: 'json',
+                cache: true,
+                success: function (data) {
+                    this.setState({weatherData: data});
+                }.bind(this),
+                error: function (xhr, status, err)  {
+                    console.log(url, err.toString());
+                }.bind(this)
+            });
+        }
+    },
     render: function()  {
         return (
             <a
                 href="#"
+                onClick={this.getWeather}
                 className="GetWeatherBtn">
                 CHECK WEATHER
             </a>
@@ -73,15 +157,24 @@ const GetWeatherBtn = React.createClass({
     }
 });
 
-const RootBlock = React.createClass({
+const InputStatus = React.createClass({
     render: function()  {
         return (
-            <div>
-                <Navbar />
-                <EnterLocation />
+            <div className="InputStatus">
+                Latitude: {this.props.lat}
+                Longitude: {this.props.long}
             </div>
         )
     }
 });
 
+const SpinnerBlock = React.createClass({
+    render: function()  {
+        return (
+            <div className="spinnerBlock">
+                <i className="fa fa-spinner fa-pulse fa-fw"></i> Loading data...
+            </div>
+        );
+    }
+});
 export default RootBlock;
